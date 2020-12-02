32a0beb91c279588300b644f3bea1db7b0a69eb6
Sarah Patrick <sarah.e.patrick@hotmail.com>
2020-12-02 15:30:46 -0500

    Setup for About and Documentation pages

--- public/script.js
+++ public/script.js
@@ -1,6 +1,16 @@
 
-function display ()
+function about()
 {
 
-document.getElementById("text").innerHTML = "Test";
+    var aboutButton = document.getElementById("about").value;
+    document.getElementById("homepage").innerHTML = aboutButton;
+    document.getElementById("text").innerHTML = "About Page Info";
 }
+
+function doc() {
+
+    var docButton = document.getElementById("doc").value;
+    document.getElementById("homepage").innerHTML = docButton;
+    document.getElementById("text").innerHTML = "Documentation";
+}
+
